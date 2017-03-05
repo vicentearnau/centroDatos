3960fb214a115a885c23cc475cff91c5e33dddc1
equipo <equipo@equipo>
2017-03-05 06:59:33 +0100

    Empezamos

---
+++ src/javabiometric/JavaBiometric.java
@@ -0,0 +1,15 @@
+package javabiometric;
+
+import java.io.File;
+
+public class JavaBiometric {
+
+  public JavaBiometric() {
+  }
+  public static void main(String[] args) {
+	  
+    CEntityForm classFormMain = new CEntityForm();
+    classFormMain.setSize(400,700);
+    classFormMain.setVisible(true);
+  }
+}
